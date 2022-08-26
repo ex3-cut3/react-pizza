81a246714d6533f987ee2b1dc903d5c12bbbb8bb
exe.cute <90452860+Max211922@users.noreply.github.com>
2022-08-26 23:56:11 +0300

    Update Payment.tsx

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,4 +1,15 @@
+import {useEffect} from "react";
+import {useAppSelector} from "../hooks/useRedux";
+import {selectCart} from "../store/Cart/selectors";
+import {useNavigate} from "react-router-dom";
+
 const Payment = () => {
+    const {totalItems} = useAppSelector(selectCart);
+    const navigate = useNavigate();
+    useEffect(() => {
+        totalItems === 0 && navigate('/cart')
+    }, []);
+
     return (
         <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '15px'}}>
             <h2 style={{textAlign: 'center', padding: '5px 10px'}}>In honour of the greatest actor and the man of all times, Billy Herrington</h2>
