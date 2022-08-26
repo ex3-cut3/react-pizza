66f774bed4db75eb053e7c758785a1cfb4048ff8
exe.cute <90452860+Max211922@users.noreply.github.com>
2022-08-26 23:20:53 +0300

    Added .env

--- src/store/Pizza/PizzaAC.ts
+++ src/store/Pizza/PizzaAC.ts
@@ -1,32 +1,30 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios, { AxiosError } from "axios";
-import { Pizza } from "../../components/Layout/PizzaBlock/PizzaBlockList";
-import { RejectedWithValueActionFromAsyncThunk } from "@reduxjs/toolkit/dist/matchers";
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import axios, {AxiosError} from "axios";
+import {Pizza} from "../../components/Layout/PizzaBlock/PizzaBlockList";
+import {RejectedWithValueActionFromAsyncThunk} from "@reduxjs/toolkit/dist/matchers";
 import {SortOptions} from "../Navigation/NavigationTypes";
 
 export type FetchPizzasArgs = {
-  sortOptions: SortOptions;
-  page: number;
-  limit: number;
+    sortOptions: SortOptions;
+    page: number;
+    limit: number;
 };
 
-export const fetchPizzas = createAsyncThunk<
-  Pizza[] | RejectedWithValueActionFromAsyncThunk<any>,
-  FetchPizzasArgs
->("pizzas/fetchPizzas", async ({ sortOptions, page, limit }, thunkAPI) => {
-  try {
-    const { category, sortBy, order, searchQuery } = sortOptions;
+export const fetchPizzas = createAsyncThunk<Pizza[] | RejectedWithValueActionFromAsyncThunk<any>,
+    FetchPizzasArgs>("pizzas/fetchPizzas", async ({sortOptions, page, limit}, thunkAPI) => {
+    try {
+        const {category, sortBy, order, searchQuery} = sortOptions;
 
-    const computedCategory = category !== 0 ? category : "";
-    const response = await axios.get<Pizza[]>(
-      `${process.env.REACT_APP_API_URL}?sortBy=${sortBy}&category=${computedCategory}&order=${order}`
-    ); // &page=${page}&limit=${limit}
-    return { pizzas: response.data, page, limit };
-  } catch (e: unknown) {
-    // console.log(e);
-    const { response } = e as AxiosError;
-    return thunkAPI.rejectWithValue(
-      "Unable to load pizzas 😒! " + response?.data
-    );
-  }
+        const computedCategory = category !== 0 ? category : "";
+        const response = await axios.get<Pizza[]>(
+            `${process.env.REACT_APP_API_URL}?sortBy=${sortBy}&category=${computedCategory}&order=${order}`
+        ); // &page=${page}&limit=${limit}
+        return {pizzas: response.data, page, limit};
+    } catch (e: unknown) {
+        // console.log(e);
+        const {response} = e as AxiosError;
+        return thunkAPI.rejectWithValue(
+            "Unable to load pizzas 😒! " + response?.data
+        );
+    }
 });
